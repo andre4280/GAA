4d5e8e4f90ed556040880c814d34ed1044566ecc
UserPL135Pc05 <UserPL135Pc05@SLFRPL135PC05.ensino.atec.local>
2016-10-26 10:41:00 +0100

    Introdução.

---
+++ src/principal.java
@@ -0,0 +1,13 @@
+
+public class principal {
+
+	public principal() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println ("Olá aleijado.");
+	}
+
+}
